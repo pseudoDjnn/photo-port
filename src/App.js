afe10224076d2ef33a8aedc448bba88801c6576d
p5eudoDjnn <alexander.havers@gmail.com>
2022-11-20 17:38:23 -0500

    added jsx navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 // import logo from "./logo.svg";
+import React from "react";
+// import logo from "./logo.svg";
+import About from "./components/About";
+import Nav from "./components/Nav";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav></Nav>
+      <main>
+        <About></About>
+      </main>
     </div>
   );
 }
