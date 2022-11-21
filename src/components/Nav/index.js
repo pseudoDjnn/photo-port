7018e63e47f99f3e5e0dc36389edfc7238f15efc
p5eudoDjnn <alexander.havers@gmail.com>
2022-11-21 13:22:24 -0500

    added tests

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -19,10 +19,11 @@ function categorySelected(name) {
 
 function Nav() {
   return (
-    <header>
+    <header className="flex-row px-1">
       <h2>
-        <a href="/">
+        <a data-testid="link" href="/">
           <span role="img" aria-label="camera">
+            {" "}
             📸
           </span>{" "}
           Oh Snap!
@@ -31,7 +32,9 @@ function Nav() {
       <nav>
         <ul className="flex-row">
           <li className="mx-2">
-            <a href="#about">About Me</a>
+            <a data-testid="about" href="#about">
+              About Me
+            </a>
           </li>
           <li>
             <span>Contact</span>
